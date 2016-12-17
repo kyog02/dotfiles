5effa35d968c52d643207f18ae5cb4ef939c0011
kyog02 <pleage55@yahoo.co.jp>
2016-12-17 17:01:58 +0900

    add gtags.conf and Partial modification

--- setup.sh
+++ setup.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
+#元はHOMEディレクトリにあるdotfileにシンボリックリンクを設定
  DOT_FILES=(.bashrc .bash_profile .vimrc )
 
 for file in ${DOT_FILES[@]}
 do
 	ln -s $HOME/dotfiles/$file $HOME/$file
 done
+
+#gtagsは元はglobalディレクトリに存在していたため、当箇所にシンボリックリンクを設定
+ gtags=gtags.conf
+ ln -s $HOME/dotfiles/$gtags $HOME/global
+
+
+
