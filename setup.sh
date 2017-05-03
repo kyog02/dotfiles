1d4005a22be4d773ad6c375d3424c1f1a8167293
kyog02 <pleage55@yahoo.co.jp>
2017-05-04 02:44:17 +0900

    Fixed setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-#元はHOMEディレクトリにあるdotfileにシンボリックリンクを設定
- DOT_FILES=(.bashrc .bash_profile .vimrc )
+#元々HOMEディレクトリにあるdotfileにシンボリックリンクを設定
+ DOT_FILES=(.bashrc .bash_profile .vimrc .vim .tmux.conf .atom .gitconfig .gitignore)
 
 for file in ${DOT_FILES[@]}
 do
@@ -10,7 +10,7 @@ done
 
 #gtagsは元はglobalディレクトリに存在していたため、当箇所にシンボリックリンクを設定
  gtags=gtags.conf
- ln -s $HOME/dotfiles/$gtags $HOME/global
+ ln -s $HOME/dotfiles/$gtags $HOME/global/$gtags
 
 
 
